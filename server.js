49574b82041a20d91cfb354753d2a86e94ef1277
Akeia M <mccollumakeia@outlook.com>
2022-01-26 22:14:14 -0500

    created server.js and routes folder

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port.
+var PORT = 
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//setup public folder for images, js, css
+app.use(express.static('public'));
+
+
+//ROUTES
+
+//API 
+require("./routes/apiroutes")(app);
+
+// HTML 
+require("./routes/htmlroutes")(app);
+
+
+
+// LISTENER
+// The below code effectively "starts" our server
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});

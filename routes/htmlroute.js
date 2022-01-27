07c0c0678cf43bf5b5a56de374f1d7cd3083299d
Akeia M <mccollumakeia@outlook.com>
2022-01-26 22:26:56 -0500

    updated js htmlroute

---
+++ routes/htmlroute.js
@@ -0,0 +1,15 @@
+//  Dependency
+var path = require("path");
+
+
+module.exports = function (app) {
+                    app.get('notes', function (req, res){
+                                        res.sendFile(path.join("../public/notes.html"));
+                    });
+}
+
+// If no matching route is found default to home
+app.get("*", function (req, res) {
+                    res.sendFile(path.join("../public/index.html"));
+
+});

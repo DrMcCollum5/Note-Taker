fae0d247b9136b96bce11927761c5f345667490d
Akeia M <mccollumakeia@outlook.com>
2022-01-27 21:50:29 -0500

    added listener to server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
-
-
-// Tells node that we are creating an "express" server
 const app = express();
+
 
 // Sets an initial port.
 const PORT = 3000
 
 // Sets up the Express app to handle data parsing
+// app.use() is middleware funciton
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -28,6 +28,4 @@ require("./routes/htmlroutes")(app);
 // LISTENER
 // The below code effectively "starts" our server
 
-app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
-});
+app.listen(PORT, () =>  console.log("App listening on PORT: " + PORT));

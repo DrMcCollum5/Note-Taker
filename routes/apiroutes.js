94e68ba13663a34f5b1d5c816257b2ede2bfa23f
DrMcCollum5 <94315015+DrMcCollum5@users.noreply.github.com>
2022-02-03 17:24:48 -0500

    updated routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,16 +2,36 @@ const router = require("express").Router();
 
 const { v4: uuidv4 } = require("uuid");
 const {
-                    readFromFile,
-                    readAndAppend,
-                    writeToFile,
-                  } = require('../helpers/fsUtils');
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../helpers/fsUtils");
 
 // router.get
 // router.post
 // router.delete
 
+router.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((notes) => res.json(JSON.parse(notes)));
+});
 
-router.get('/', (req, res) => {
-                    readFromFile('./db/db.json').then((notes) => res.json(JSON.parse(notes)));
-                  });
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text, } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding Note');
+  }
+});
+module.exports=router;
--- server.js
+++ server.js
@@ -1,37 +1,33 @@
 const express = require("express");
-const app = express();
-const {clog}= require('./middleware/clog')
-const apiroutes = require('./routes/apiroutes')
-const htmlroute = require('./routes/htmlroute')
+
+const { clog } = require("./middleware/clog");
+const apiroutes = require("./routes/apiroutes");
+const htmlroutes = require("./routes/htmlroutes");
 
 // Sets an initial port.
 const PORT = process.env.PORT || 3000;
-
+const app = express();
 // Sets up the Express app to handle data parsing
 // app.use() is middleware funciton
-app.use(clog)
+app.use(clog);
 
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //setup public folder for images, js, css
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 //ROUTES
 
-//API 
+//API
 //require("./routes/apiroutes")(app);
-app.use('/api', apiroutes);
-app.use('/', htmlroute)
+app.use("/api", apiroutes);
+app.use("/", htmlroutes);
 
-// HTML 
+// HTML
 //require("./routes/htmlroutes")(app);
-
-
 
 // LISTENER
 // The below code effectively "starts" our server
 
-app.listen(PORT, () =>  console.log("App listening on PORT: " + PORT));
+app.listen(PORT, () => console.log("App listening on PORT: " + PORT));
